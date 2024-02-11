7ac07dcc59dcf2c260339006eed56e9ba9cc4009
Bera Senol <berasenol@icloud.com>
2024-02-11 14:08:41 +0100

    Navbar & Icon Config

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <UHorizontalNavigation
+    :links="links"
+    class="border-b border-gray-200 dark:border-gray-800"
+  />
+  <slot />
+</template>
+
+<script setup>
+const route = useRoute();
+
+const links = [
+  [
+    {
+      label: "Home",
+      icon: "i-heroicons-home",
+      to: "/home",
+    },
+  ],
+  [
+    {
+      label: "Tables",
+      icon: "material-symbols:table-restaurant-outline-rounded",
+      to: "/tables",
+    },
+  ],
+  [
+    {
+      label: "Tickets",
+      icon: "material-symbols:receipt-long-outline-rounded",
+      to: "/tickets",
+    },
+  ],
+  [
+    {
+      label: "Settings",
+      icon: "ion:settings-outline",
+      to: "/settings",
+    },
+  ],
+];
+</script>
---
+++ pages/tables.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>TABLES</h1>
+  </div>
+</template>
